0b657d33d4d8386a9f91f1a200e513c10d728e92
aristata <aristatait@gmail.com>
2021-10-25 09:48:28 +0900

    77p 테스트 에러

--- src/test/java/com/tistory/aristatait/web/HelloControllerTest.java
+++ src/test/java/com/tistory/aristatait/web/HelloControllerTest.java
@@ -6,8 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * 테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행한다.
@@ -22,12 +27,27 @@ public class HelloControllerTest {
     private MockMvc mvc; // 웹 API 를 테스트 할 때 사용하는 클래스, 이 클래스를 통해 HTTP GET, POST 등에 대한 API 테스트를 할 수 있다.
 
     @Test
-    public void test1() throws Exception {
+    public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
-        mvc.perform(MockMvcRequestBuilders.get("/hello")) // MockMvc 를 통해 /hello 주소로 HTTP GET 요청을 한다.
-                .andExpect(MockMvcResultMatchers.status().isOk()) // 위 행의 결과를 검증한다. Header 의 Status 가 200인지 아닌지 검증
+        mvc.perform(get("/hello")) // MockMvc 를 통해 /hello 주소로 HTTP GET 요청을 한다.
+                .andExpect(status().isOk()) // 위 행의 결과를 검증한다. Header 의 Status 가 200인지 아닌지 검증
                 .andExpect(MockMvcResultMatchers.content().string(hello)); // Body 의 content 가 "hello" 인지 검증 한다.
 
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                        get("/hello/dto")
+                                .param("name", name)
+                                .param("amount", String.valueOf(amount))
+                )
+                .andExpect(status().isOk())
+                .andExpect((ResultMatcher) jsonPath("$.name", is(name)))
+                .andExpect((ResultMatcher) jsonPath("$.amount", is(amount)));
     }
 }
