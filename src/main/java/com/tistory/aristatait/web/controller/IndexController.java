cfea2dc26bee351572ef78ef732034abf0846c71
aristata <aristatait@gmail.com>
2021-11-19 15:12:02 +0900

    게시글 수정

--- src/main/java/com/tistory/aristatait/web/controller/IndexController.java
+++ src/main/java/com/tistory/aristatait/web/controller/IndexController.java
@@ -1,10 +1,12 @@
 package com.tistory.aristatait.web.controller;
 
 import com.tistory.aristatait.service.posts.PostsService;
+import com.tistory.aristatait.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * 최초 작성자 castlepeople
@@ -24,5 +26,16 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(
+            @PathVariable Long id,
+            Model model
+    ) {
+        PostsResponseDto responseDto = postsService.findById(id);
+        model.addAttribute("post", responseDto);
+
+        return "posts-update";
     }
 }
--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -4,6 +4,10 @@ var main = {
         $('#btn-save').on('click', function () {
             _this.save();
         });
+
+        $('#btn-update').on('click', function () {
+            _this.update();
+        })
     },
     save: function () {
         var data = {
@@ -21,6 +25,27 @@ var main = {
         }).done(function () {
             alert('글이 등록되었습니다.');
             window.location.href = '/'; //글 등록이 성공하면 메인페이지로 이동
+        }).fail(function (e) {
+            alert(JSON.stringify(e));
+        })
+    },
+    update: function () {
+        const data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        const id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function () {
+            alert('글이 수정되었습니다.');
+            window.location.href = "/";
         }).fail(function (e) {
             alert(JSON.stringify(e));
         })
