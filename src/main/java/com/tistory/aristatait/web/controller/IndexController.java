d91771d50402917a5b820cd62843a966c3b646d5
aristata <aristatait@gmail.com>
2021-11-19 13:47:19 +0900

    mustache 로 화면 만들기

---
+++ src/main/java/com/tistory/aristatait/web/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.tistory.aristatait.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * 최초 작성자 castlepeople
+ * 최초 작성일 2021-11-19
+ **/
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
